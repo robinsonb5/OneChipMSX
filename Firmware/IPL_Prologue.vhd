be6b21a1d32743cc7e572d8f203caa9aaf31c993
Alastair M. Robinson <robinsonb5@talk21.com>
2014-08-12 00:54:02 +0100

    Added ROM building infrastructure.

---
+++ Firmware/IPL_Prologue.vhd
@@ -0,0 +1,56 @@
+-- 
+-- iplrom.vhd
+--   initial program loader for Cyclone & EPCS (Altera)
+--   Revision 1.00
+-- 
+-- Copyright (c) 2006 Kazuhiro Tsujikawa (ESE Artists' factory)
+-- All rights reserved.
+-- 
+-- Redistribution and use of this source code or any derivative works, are 
+-- permitted provided that the following conditions are met:
+--
+-- 1. Redistributions of source code must retain the above copyright notice, 
+--    this list of conditions and the following disclaimer.
+-- 2. Redistributions in binary form must reproduce the above copyright 
+--    notice, this list of conditions and the following disclaimer in the 
+--    documentation and/or other materials provided with the distribution.
+-- 3. Redistributions may not be sold, nor may they be used in a commercial 
+--    product or activity without specific prior written permission.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
+-- TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+-- PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+-- CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+-- EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+-- PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+-- OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+-- WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+-- OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+-- ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--
+-- 31th,March,2008
+-- patch caro for load KANJI BASIC from SD/MMC
+--
+-- 13th,April,2008
+-- patch t.hara for load SLOT0-1/SLOT0-3 from SD/MMC
+--
+-- 29th,April,2008
+-- patch caro for load compressed BIOS from EPCS4
+--
+
+LIBRARY IEEE;
+	USE IEEE.STD_LOGIC_1164.ALL;
+	USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+ENTITY IPLROM IS
+	PORT (
+		CLK		: IN STD_LOGIC;
+		ADR		: IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+		DBI		: OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+	);
+END IPLROM;
+
+ARCHITECTURE RTL OF IPLROM IS
+	TYPE ROM_TYPE IS ARRAY (0 TO 767) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
+	CONSTANT IPL_DATA : ROM_TYPE := (
